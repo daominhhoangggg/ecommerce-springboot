b106a46b047f3ae823331d1843ad793bd4c2f159
Minh Hoang <daominhhoangggg@gmail.com>
2024-07-11 13:48:53 +0700

    Get category

--- src/main/java/com/ecommerce/app/controllers/productController.java
+++ src/main/java/com/ecommerce/app/controllers/productController.java
@@ -1,9 +1,13 @@
 package com.ecommerce.app.controllers;
 
 import com.ecommerce.app.models.Product;
+import com.ecommerce.app.models.ResponseObject;
 import com.ecommerce.app.repositories.ProductRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,5 +24,18 @@ public class productController {
     @GetMapping("")
     List<Product> getAllProducts() {
         return repository.findAllProducts();
+    }
+
+    @GetMapping("/category")
+    ResponseEntity<ResponseObject> getCategory(@RequestParam("category") String category) {
+        List<Product> foundProducts;
+        if("all".equalsIgnoreCase(category)) {
+            foundProducts = repository.findAllProducts();
+        } else {
+            foundProducts = repository.findProductsByCategory(category);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(
+                new ResponseObject("ok", "Query by category successfully", foundProducts)
+        );
     }
 }
--- src/main/java/com/ecommerce/app/repositories/ProductRepository.java
+++ src/main/java/com/ecommerce/app/repositories/ProductRepository.java
@@ -3,10 +3,15 @@ package com.ecommerce.app.repositories;
 import com.ecommerce.app.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query("FROM Product")
     List<Product> findAllProducts();
+
+    @Query("FROM Product WHERE category = :category")
+    List<Product> findProductsByCategory(@Param("category") String category);
 }
