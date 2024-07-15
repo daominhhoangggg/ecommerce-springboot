1070e1bfea89838af64e8454e5f5d063f35408c2
Minh Hoang <daominhhoangggg@gmail.com>
2024-07-15 09:07:19 +0700

    CORS Config

--- src/main/java/com/ecommerce/app/controllers/productController.java
+++ src/main/java/com/ecommerce/app/controllers/productController.java
@@ -4,7 +4,6 @@ import com.ecommerce.app.models.Product;
 import com.ecommerce.app.models.ResponseObject;
 import com.ecommerce.app.repositories.ProductRepository;
 import com.ecommerce.app.services.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
